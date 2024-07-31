b58ffef15c2a7a8cfce6960fb6350ba76aaa4d8e
Dromen2001 <dromen2001@gmail.com>
2024-07-31 09:06:27 -0700

    added about and venue index and some styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('venues/', views.venue_index, name='venue-index'),
 ]
