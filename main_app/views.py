d210ce6ceb71d69c3cb53a9d110a4542ddb22953
jtraya <jtraya7@yahoo.com>
2024-08-01 17:37:50 -0500

    added auth decorators and mixins

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 
 from .models import Venue, Event
@@ -12,7 +14,7 @@ from .models import Venue, Event
 class Home(LoginView):
     template_name = 'home.html'
 
-class VenueCreate(CreateView):
+class VenueCreate(LoginRequiredMixin, CreateView):
     model = Venue
     fields = ['name', 'location', 'description']
 
@@ -20,29 +22,29 @@ class VenueCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class VenueUpdate(UpdateView):
+class VenueUpdate(LoginRequiredMixin, UpdateView):
     model = Venue
     fields = ['name', 'location', 'description']
 
-class VenueDelete(DeleteView):
+class VenueDelete(LoginRequiredMixin, DeleteView):
     model = Venue
     success_url = '/venues/'
 
-class EventCreate(CreateView):
+class EventCreate(LoginRequiredMixin, CreateView):
     model = Event
     fields = ['name', 'producer', 'date_time', 'room_area', 'attendees', 'performances']
 
-class EventList(ListView):
+class EventList(LoginRequiredMixin, ListView):
     model = Event
 
-class EventDetail(DetailView):
+class EventDetail(LoginRequiredMixin, DetailView):
     model = Event
 
-class EventUpdate(UpdateView):
+class EventUpdate(LoginRequiredMixin, UpdateView):
     model = Event
     fields = ['name', 'producer', 'date_time', 'room_area', 'attendees', 'performances']
 
-class EventDelete(DeleteView):
+class EventDelete(LoginRequiredMixin, DeleteView):
     model = Event
     success_url = '/events/'
 
@@ -50,21 +52,23 @@ class EventDelete(DeleteView):
 def about(request):
     return render(request, 'about.html')
 
-
-
+@login_required
 def venue_index(request):
     venues = Venue.objects.all()
     return render(request, 'venues/index.html', {'venues': venues})
 
+@login_required
 def venue_detail(request, venue_id):
     venue = Venue.objects.get(id=venue_id)
     events_not_in_venue = Event.objects.exclude(id__in = venue.events.all().values_list('id'))
     return render(request, 'venues/venue_details.html', {'venue': venue, 'events': events_not_in_venue})
 
+@login_required
 def associate_event(request, venue_id, event_id):
     Venue.objects.get(id=venue_id).events.add(event_id)
     return redirect('venue-detail', venue_id=venue_id)
 
+@login_required
 def remove_event(request, venue_id, event_id):
     Venue.objects.get(id=venue_id).events.remove(event_id)
     return redirect('venue-detail', venue_id=venue_id)
